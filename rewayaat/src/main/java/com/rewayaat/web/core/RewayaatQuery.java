cd20306e69d79fde67520346d262b46b01d0f891
muntazir <muntazir@192.168.43.49>
2017-05-05 18:56:02 -0400

    fix compilation error

--- rewayaat/src/main/java/com/rewayaat/web/core/RewayaatQuery.java
+++ rewayaat/src/main/java/com/rewayaat/web/core/RewayaatQuery.java
@@ -15,7 +15,6 @@ public class RewayaatQuery {
         this.query = query;
     }
 
-<<<<<<< HEAD
 	public String query() {
 		System.out.println("Modifying query: " + query);
 		StringBuilder newQuery = new StringBuilder();
@@ -48,39 +47,6 @@ public class RewayaatQuery {
 		System.out.println("Modification complete, new query is: " + newQuery.toString());
 		return newQuery.toString();
 	}
-=======
-    public String query() {
-        System.out.println("Modifying query: " + query);
-        StringBuilder newQuery = new StringBuilder();
-        String[] terms = query.split("\"?([\\s\\xA0]|$)(?=(([^\"]*\"){2})*[^\"]*$)\"?");
-        for (String term : terms) {
-            if (term.length() > 0) {
-                if (!term.matches(".*~[0-9].*") && !isProbablyArabic(term)) {
-                    // term does not have fuzziness applied to it yet...
-                    if (term.endsWith("\"") && term.startsWith("\"")) {
-                        // term is a phrase, add slop amount based on total
-                        // phrase length.
-                        int slopAmount = (int) (term.split(" ").length * 0.3) + 2;
-                        System.out.println("Using slop value of " + slopAmount + " for phrase:\n" + term);
-                        newQuery.append(term + "~" + slopAmount + " ");
-                    } else {
-                        // add default non-phrase fuzziness amount
-                        if (term.length() > 5) {
-                            newQuery.append(term + "~2 ");
-                        } else {
-                            newQuery.append(term + "~1 ");
-                        }
-                    }
-                } else {
-                    // term already has fuzziness applied..
-                    newQuery.append(term + " ");
-                }
-            }
-        }
-        System.out.println("Modification complete, new query is: " + newQuery.toString());
-        return newQuery.toString();
-    }
->>>>>>> refs/remotes/origin/master
 
     public static boolean isProbablyArabic(String s) {
         for (int i = 0; i < s.length();) {
