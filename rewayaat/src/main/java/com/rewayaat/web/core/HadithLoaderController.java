5e04c633d32975b0bc8cf91321135fd89bdd9a25
root <root@zir0>
2017-05-05 23:03:58 -0400

    fix checkstyle errors

--- rewayaat/src/main/java/com/rewayaat/web/core/HadithLoaderController.java
+++ rewayaat/src/main/java/com/rewayaat/web/core/HadithLoaderController.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.log4j.Logger;
-import org.elasticsearch.action.index.IndexResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -16,29 +15,32 @@ import com.rewayaat.web.HomeController;
 import com.rewayaat.web.config.ClientProvider;
 import com.rewayaat.web.data.hadith.HadithObject;
 
+/**
+ * For loading hadith into the system.
+ */
 @Controller
 public class HadithLoaderController {
 
-	private static Logger log = Logger.getLogger(HadithLoaderController.class.getName());
+    private static Logger log = Logger.getLogger(HadithLoaderController.class.getName());
 
-	@RequestMapping(value = "/load", method = RequestMethod.GET)
-	public final ResponseEntity<String> loadHadith() {
+    @RequestMapping(value = "/load", method = RequestMethod.GET)
+    public final ResponseEntity<String> loadHadith() {
 
-		log.info("Entered hadith controller, loading hadith on class path into the database");
-		ClassLoader classLoader = HomeController.class.getClassLoader();
-		try {
-			ObjectMapper mapper = new ObjectMapper();
-			String result = IOUtils.toString(classLoader.getResourceAsStream("rewayaat.json"));
-			HadithObject[] obs = mapper.readValue(result, HadithObject[].class);
-			for (HadithObject hadithInfo : obs) {
-				byte[] json = mapper.writeValueAsBytes(hadithInfo);
-				ClientProvider.instance().getClient()
-						.prepareIndex(ClientProvider.INDEX, ClientProvider.TYPE).setSource(json).get();
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-			return new ResponseEntity<String>("Error", HttpStatus.BAD_REQUEST);
-		}
-		return new ResponseEntity<String>("Success", HttpStatus.OK);
-	}
+        log.info("Entered hadith controller, loading hadith on class path into the database");
+        ClassLoader classLoader = HomeController.class.getClassLoader();
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            String result = IOUtils.toString(classLoader.getResourceAsStream("rewayaat.json"));
+            HadithObject[] obs = mapper.readValue(result, HadithObject[].class);
+            for (HadithObject hadithInfo : obs) {
+                byte[] json = mapper.writeValueAsBytes(hadithInfo);
+                ClientProvider.instance().getClient().prepareIndex(ClientProvider.INDEX, ClientProvider.TYPE)
+                        .setSource(json).get();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new ResponseEntity<String>("Error", HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<String>("Success", HttpStatus.OK);
+    }
 }
--- rewayaat/src/main/java/com/rewayaat/web/core/RewayaatQuery.java
+++ rewayaat/src/main/java/com/rewayaat/web/core/RewayaatQuery.java
@@ -1,11 +1,12 @@
 package com.rewayaat.web.core;
 
 /**
- * Uses Elastic Search Query Syntax to Modify user queries 
- * to improve search results. This is done mostly by removing
- * weird characters, adding <a href="https://www.elastic.co/guide/en/elasticsearch/guide/current/fuzziness.html">fuzziness</a>,
- * and adding <a href="https://www.elastic.co/guide/en/elasticsearch/guide/current/slop.html">slop</a> to phrases.
- * See {@linkplain RewayaatQueryTest} for expected behavior.
+ * Uses Elastic Search Query Syntax to Modify user queries to improve search
+ * results. This is done mostly by removing weird characters, adding <a href=
+ * "https://www.elastic.co/guide/en/elasticsearch/guide/current/fuzziness.html">fuzziness</a>,
+ * and adding <a href=
+ * "https://www.elastic.co/guide/en/elasticsearch/guide/current/slop.html">slop</a>
+ * to phrases. See {@linkplain RewayaatQueryTest} for expected behavior.
  */
 public class RewayaatQuery {
 
@@ -15,38 +16,38 @@ public class RewayaatQuery {
         this.query = query;
     }
 
-	public String query() {
-		System.out.println("Modifying query: " + query);
-		StringBuilder newQuery = new StringBuilder();
-		// splits query by all spaces that are not enclosed by double quotes
-		String[] terms = query.split("[\\s\\xA0]+(?=([^\"]*\"[^\"]*\")*[^\"]*$)");
-		for (String term : terms) {
-			if (term.length() > 0) {
-				if (!term.matches(".*~[0-9].*") && !isProbablyArabic(term)) {
-					// term does not have fuzziness applied to it yet...
-					if (term.startsWith("\"")) {
-						// term is a phrase, add slop amount based on total
-						// phrase length.
-						int slopAmount = (int) (term.split(" ").length * 0.3) + 2;
-						System.out.println("Using slop value of " + slopAmount + " for phrase:\n" + term);
-						newQuery.append(term + "~" + slopAmount + " ");
-					} else {
-						// add default non-phrase fuzziness amount
-						if (term.length() > 5) {
-							newQuery.append(term + "~2 ");
-						} else {
-							newQuery.append(term + "~1 ");
-						}
-					}
-				} else {
-					// term already has fuzziness applied..
-					newQuery.append(term + " ");
-				}
-			}
-		}
-		System.out.println("Modification complete, new query is: " + newQuery.toString());
-		return newQuery.toString();
-	}
+    public String query() {
+        System.out.println("Modifying query: " + query);
+        StringBuilder newQuery = new StringBuilder();
+        // splits query by all spaces that are not enclosed by double quotes
+        String[] terms = query.split("[\\s\\xA0]+(?=([^\"]*\"[^\"]*\")*[^\"]*$)");
+        for (String term : terms) {
+            if (term.length() > 0) {
+                if (!term.matches(".*~[0-9].*") && !isProbablyArabic(term)) {
+                    // term does not have fuzziness applied to it yet...
+                    if (term.startsWith("\"")) {
+                        // term is a phrase, add slop amount based on total
+                        // phrase length.
+                        int slopAmount = (int) (term.split(" ").length * 0.3) + 2;
+                        System.out.println("Using slop value of " + slopAmount + " for phrase:\n" + term);
+                        newQuery.append(term + "~" + slopAmount + " ");
+                    } else {
+                        // add default non-phrase fuzziness amount
+                        if (term.length() > 5) {
+                            newQuery.append(term + "~2 ");
+                        } else {
+                            newQuery.append(term + "~1 ");
+                        }
+                    }
+                } else {
+                    // term already has fuzziness applied..
+                    newQuery.append(term + " ");
+                }
+            }
+        }
+        System.out.println("Modification complete, new query is: " + newQuery.toString());
+        return newQuery.toString();
+    }
 
     public static boolean isProbablyArabic(String s) {
         for (int i = 0; i < s.length();) {
