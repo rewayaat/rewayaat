5c930daa85f25c03e7a70a9a75f3551d159fab67
root <root@zir0>
2017-10-21 15:01:03 -0400

    Fix deprecated method call bug

--- rewayaat/src/main/java/com/rewayaat/loader/oldhdp/OldHDPLoader.java
+++ rewayaat/src/main/java/com/rewayaat/loader/oldhdp/OldHDPLoader.java
@@ -3,7 +3,23 @@ package com.rewayaat.loader.oldhdp;
 public class OldHDPLoader {
 
     public static void main(String[] args) throws Exception {
-        Thread t = new OldHDPWorker(6000, 6010);
+        Thread t = new OldHDPWorker(2, 1000);
         t.start();
+        Thread a = new OldHDPWorker(1001, 2010);
+        a.start();
+        Thread d = new OldHDPWorker(2011, 4000);
+        d.start();
+        Thread g = new OldHDPWorker(4001, 6000);
+        g.start();
+        Thread h = new OldHDPWorker(6001, 8000);
+        h.start();
+        Thread j = new OldHDPWorker(8001, 10000);
+        j.start();
+        Thread k = new OldHDPWorker(10001, 12000);
+        k.start();
+        Thread m = new OldHDPWorker(12001, 14000);
+        m.start();
+        Thread n = new OldHDPWorker(14001, 15708);
+        n.start();
     }
 }
