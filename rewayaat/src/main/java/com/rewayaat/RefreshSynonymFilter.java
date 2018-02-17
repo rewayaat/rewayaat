a2b266de18613b2e9526c62dbb8057c456fb97c5
Muntazir Fadhel <muntazirfadhel@gmail.com>
2018-02-17 01:25:46 -0500

    Use private ip address

--- rewayaat/src/main/java/com/rewayaat/RefreshSynonymFilter.java
+++ rewayaat/src/main/java/com/rewayaat/RefreshSynonymFilter.java
@@ -57,7 +57,7 @@ public class RefreshSynonymFilter {
             }
             log.info("Current index name is: " + currIndexName);
 
-            // create backup of current index
+            // create backup of current index at /var/lib/docker/devicemapper/mnt/b43a07171255604a89772f76ed043b5a40dcb5331a84bcc9e410e48ee50ac747/rootfs/rewayaat_backup.json
             Runtime rt = Runtime.getRuntime();
             Process pr = rt.exec("elasticdump   --input=http://" + ClientProvider.host + ":9200/" + currIndexName + "   --output=/rewayaat_backup.json   --type=data");
             pr.waitFor();
