9fb71c94fb90b0e291cfb2c89b45565051effa86
Muntazir Fadhel <mfadhel@hadii.ca>
2024-04-15 21:10:05 +0300

    Fix docker push

--- push-image.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-export PATH=$PATH:$HOME/.local/bin
-
-echo "Pushing $IMAGE_NAME:$BUILD_NUMBER"
-docker tag $IMAGE_NAME:latest "$REMOTE_IMAGE_URL:$BUILD_NUMBER"
-docker push "$REMOTE_IMAGE_URL:$BUILD_NUMBER"
-echo "Pushed $IMAGE_NAME:$BUILD_NUMBER"
