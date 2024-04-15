b9a4269fab782901dd94875ce94f26933c2b91f6
rewayaat <rewayaat.org@gmail.com>
2024-04-15 20:01:17 +0300

    Setup for digital ocean migration (#58)
    * Setup for digital ocean migration
    
    * Fix checkstyle errors
    
    * Cleanup build script
    
    * Fix up build scripts
    
    * Fix up build scripts
    
    ---------
    
    Co-authored-by: Muntazir Fadhel <mfadhel@hadii.ca>

---
+++ push-image.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+export PATH=$PATH:$HOME/.local/bin
+
+echo "Pushing $IMAGE_NAME:$BUILD_NUMBER"
+docker tag $IMAGE_NAME:latest "$REMOTE_IMAGE_URL:$BUILD_NUMBER"
+docker push "$REMOTE_IMAGE_URL:$BUILD_NUMBER"
+echo "Pushed $IMAGE_NAME:$BUILD_NUMBER"
--- src/main/java/com/rewayaat/RewayaatApplication.java
+++ src/main/java/com/rewayaat/RewayaatApplication.java
@@ -1,7 +1,5 @@
 package com.rewayaat;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -10,11 +8,9 @@ import org.springframework.cache.annotation.EnableCaching;
  * Main entrypoint to rewayaat app.
  */
 @EnableCaching
-@SpringBootApplication(scanBasePackages = {"com.rewayaat.config", "com.rewayaat.controllers",
-    "com.rewayaat.service"})
+@SpringBootApplication(scanBasePackages = { "com.rewayaat.config", "com.rewayaat.controllers",
+        "com.rewayaat.service" })
 public class RewayaatApplication {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(RewayaatApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(RewayaatApplication.class, args);
