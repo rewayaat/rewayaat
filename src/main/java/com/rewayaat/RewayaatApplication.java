2a96fbfaa66699d7cccd2bdb5a5b80f338ead59f
Muntazir Fadhel <mfadhel@hadii.ca>
2024-04-15 19:48:05 +0300

    Fix checkstyle errors

--- src/main/java/com/rewayaat/RewayaatApplication.java
+++ src/main/java/com/rewayaat/RewayaatApplication.java
@@ -1,7 +1,5 @@
 package com.rewayaat;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
